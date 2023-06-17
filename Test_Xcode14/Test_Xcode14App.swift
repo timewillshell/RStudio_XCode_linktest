dc091ed8f4e9866b35cb4bc895b886e6c69a7598
Shellianne Booth <shellianne.booth@gmail.com>
2023-06-18 09:51:42 +1000

    Initial Commit

---
+++ Test_Xcode14/Test_Xcode14App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Xcode14App.swift
+//  Test_Xcode14
+//
+//  Created by Shelly Booth on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_Xcode14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
